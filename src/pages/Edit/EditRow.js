9fba0cd6af4525732a1cf370852d6505fefa1d19
rashed <x.suwaidi@gmail.com>
2022-09-03 12:43:16 +0400

    UI update

--- src/pages/Edit/EditRow.js
+++ src/pages/Edit/EditRow.js
@@ -1,16 +1,18 @@
 import { Footer } from "flowbite-react";
 
 export default function EditRow(props) {
-    return (
-      <div className="my-5">
-        <div className={`grid grid-cols-8 gap-4  ${props.spacing}`}>
-          <div className="col-start-1 col-span-2">
-            <p className="text-xl font-semibold">{props.label}</p>
-            <p className="text-sm text-gray-500 ">{props.info}</p>
-          </div>
-          <div className="col-end-7 col-span-4 max-w-md">{props.children}</div>
+  return (
+    <div className="my-5">
+      <div className={`md:flex  ${props.spacing}`}>
+        <div className="w-64 pr-10 mb-5 md:mb-0">
+          <p className="text-xl font-semibold">{props.label}</p>
+          <p className="text-sm text-gray-500 ">{props.info}</p>
         </div>
-        <Footer.Divider />
+        <div className="md:flex-grow">
+          <div className="xl:w-6/12 mx-auto md:px-0 px-2">{props.children}</div>
+        </div>
       </div>
-    );
-  }
+      <Footer.Divider />
+    </div>
+  );
+}
